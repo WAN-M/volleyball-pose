898bdfdb0c3d864749c0d6732fd5a079cb43a548
此用户无名 <20373858@buaa.edu.cn>
2023-06-27 17:41:12 +0800

    指定识别的人物

--- src/rules/digRule/armDetect.py
+++ src/rules/digRule/armDetect.py
@@ -19,30 +19,30 @@ def detect_line(list):
     return angle
 
 
-def get_armPoint(armIndex, candidate, subset):
+def get_armPoint(armIndex, candidate, person):
     list1 = [[] for _ in range(3)]
     for n in range(len(armIndex)):
-        # index = int(subset[0][armIndex[n]])
+        # index = int(person[armIndex[n]])
         # #print(index)
         # if index == -1:
         #     raise Exception("输入图像未包含手臂的全部状况")
         # x, y = candidate[index][0:2]
         # list1[n].append(x)
         # list1[n].append(y)
-        list1[n] = util.num2pos(armIndex[n], candidate, subset[0])
+        list1[n] = util.num2pos(armIndex[n], candidate, person)
         #print(list1)
     return list1
 
-def detect_arm_status(image, candidate, subset):
+def detect_arm_status(image, candidate, person):
     status = True
     arms = [2, 3, 4]
-    list1 = get_armPoint(arms, candidate, subset)
+    list1 = get_armPoint(arms, candidate, person)
     leftAngle = 0
     rightAngle = 0
     if list1 != None:
         leftAngle = detect_line(list1)
     arms = [5, 6, 7]
-    list2 = get_armPoint(arms, candidate, subset)
+    list2 = get_armPoint(arms, candidate, person)
     if list2 != None:
         rightAngle = detect_line(list2)
     if leftAngle != 0 and rightAngle != 0:
