36880b9f6a843f523285fdd174f81134e8bd2628
WAN-M <1437207980@qq.com>
2023-06-27 11:09:52 +0800

    完成手臂躯干角度判断

---
+++ src/rules/digRule/armTorsoAngle.py
@@ -0,0 +1,27 @@
+'''
+检测手臂与躯干最大角度不超过100°
+'''
+import math
+
+def judge_angle(xm, ym, x1, y1, x2, y2)->bool:
+    a = math.sqrt(math.pow((xm - x1), 2) + math.pow((ym - y1), 2))
+    b = math.sqrt(math.pow((xm - x2), 2) + math.pow((ym - y2), 2))
+    c = math.sqrt(math.pow((x1 - x2), 2) + math.pow((y1 - y2), 2))
+
+    angle = math.degrees(math.acos((c * c - a * a - b * b) / (-2 * a * b)))
+
+    return angle <= 100
+
+def detect(image, candidate, subset)->bool:
+    flag = True
+    # 判断左臂与躯干，选取5,6,11
+    if not judge_angle(candidate[subset[0][5]][0:2],
+                       candidate[subset[0][6]][0:2],
+                       candidate[subset[0][11]][0:2]):
+        flag = False
+    # 判断右臂与躯干，选取2,3,8
+    if not judge_angle(candidate[subset[0][2]][0:2],
+                       candidate[subset[0][3]][0:2],
+                       candidate[subset[0][8]][0:2]):
+        flag = False
+    return flag
