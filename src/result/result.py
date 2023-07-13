c578ee173d65b1c2a1f8184c03d8e33512fe44bd
WAN-M <1437207980@qq.com>
2023-07-13 21:48:25 +0800

    返回视频的url

--- src/result/result.py
+++ src/result/result.py
@@ -2,15 +2,16 @@ import json_tricks as json
 
 
 class CommonResult():
-    def __init__(self, code, message, data):
+    def __init__(self, code, message, data=None, video=None):
         self.code = code
         self.message = message
         self.data = data
+        self.video = video
 
     @classmethod
-    def success(self, message, data):
-        return json.dumps(CommonResult(200, message, json.dumps(data)))
+    def success(self, message, data, video=None):
+        return json.dumps(CommonResult(200, message, json.dumps(data), video))
 
     @classmethod
     def fail(self, message):
-        return json.dumps(CommonResult(500, message, None))
+        return json.dumps(CommonResult(500, message))
