2ee9b1048100dfabd8d6d3a72fe8f15127196e72
WAN-M <1437207980@qq.com>
2023-07-18 13:15:38 +0800

    增加手部检测api

--- src/utils/detect.py
+++ src/utils/detect.py
@@ -1,9 +1,13 @@
 import sys
 from pathlib import Path
 
+import numpy as np
+
 from src.models.ball import run
 from src.models.body import Body
 from src.models.common import DetectMultiBackend
+from src.models.hand import Hand
+from src.utils import util
 from src.utils.logger import Log
 from src.utils.torch_utils import select_device
 
@@ -13,6 +17,7 @@ if str(ROOT) not in sys.path:
     sys.path.append(str(ROOT))  # add ROOT to PATH
 Log.info("项目运行目录: " + str(ROOT))
 body_estimation = Body(str(ROOT / 'model/body_pose_model.pth'))
+hand_estimation = Hand(ROOT / 'model/hand_pose_model.pth')
 device = select_device()
 volleyball_model = DetectMultiBackend(str(ROOT / 'model/yolov5x.pt'), device=device, dnn=False, data='data/coco128.yaml')
 
@@ -30,3 +35,15 @@ def detect_person(image):
 
 def detect_ball(image):
     return run(volleyball_model, image)
+
+def detect_hand(image, candidate, person):
+    hands_list = util.handDetect(candidate, [person], image)
+
+    all_hand_peaks = []
+    for x, y, w, is_left in hands_list:
+        peaks = hand_estimation(image[y:y+w, x:x+w, :])
+        peaks[:, 0] = np.where(peaks[:, 0]==0, peaks[:, 0], peaks[:, 0]+x)
+        peaks[:, 1] = np.where(peaks[:, 1]==0, peaks[:, 1], peaks[:, 1]+y)
+        all_hand_peaks.append(peaks)
+
+    return all_hand_peaks
