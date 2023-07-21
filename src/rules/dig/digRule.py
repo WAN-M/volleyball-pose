abf8edda3f56f93b4b4842cbe25b6bc508a1cef9
WAN-M <1437207980@qq.com>
2023-07-21 09:23:05 +0800

    修改手臂伸直检测

--- src/rules/dig/digRule.py
+++ src/rules/dig/digRule.py
@@ -10,18 +10,19 @@ def sum_rules(images, candidates, persons, balls):
     # 需要每张图片遍历的规则
     mes = set()
     for i in range(len(images)):
-        if not armDetect.detect_arm_status(images[i], candidates[i], persons[i]):
-            mes.add("手臂没有伸直")
+        # 每张图片需要遵守的规则
         if not armTorsoAngle.detect(images[i], candidates[i], persons[i]):
             mes.add("手臂与躯干最大角度不应超过100°")
-        if arm_dis_ball(candidates[i], persons[i], balls[i]) < 2:
-            try:
-                dis = arm_dis_ball(candidates[i], persons[i], balls[i])
-                if dis < 2:
-                    if not ball_position(images[i], candidates[i], persons[i], balls[i]):
-                        mes.add("击球时球离手腕位置太远")
-            except:
-                Log.error("球未被识别")
+
+        # 击球前后需要遵守的规则
+        try:
+            if arm_dis_ball(candidates[i], persons[i], balls[i]) < 2:
+                if not ball_position(images[i], candidates[i], persons[i], balls[i]):
+                    mes.add("击球时球离手腕位置太远")
+                if not armDetect.detect_arm_status(images[i], candidates[i], persons[i]):
+                    mes.add("手臂没有伸直")
+        except:
+            Log.error("球未被识别")
 
         if len(mes) > 0:
             image = draw_messages(images[i], mes)
